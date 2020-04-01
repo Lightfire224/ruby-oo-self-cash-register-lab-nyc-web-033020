91981554f98a185b49fcdaa269b9fe82f2d37851
Elias Taveras <eliastave12@gmail.com>
2020-04-01 23:42:14 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -24,11 +24,11 @@ class CashRegister
   
   def add_item(title, price, quantity=1)
     @total += price * quantity
-    # counter = 0
-    # while counter < quantity
-    #   @items_list << title
-    #   counter += 1
-    # end
+                              # counter = 0
+                              # while counter < quantity
+                              #   @items_list << title
+                              #   counter += 1
+                              # end
     
     quantity.times do
       @items_list << title
